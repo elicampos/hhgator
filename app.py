f40effff1c5f67809fb59f83b57cd491db2a9449
Eli Campos <elicamposbusiness@gmail.com>
2023-10-22 03:17:22 -0400

    Update backend

--- app.py
+++ app.py
@@ -5,32 +5,39 @@ import os
 import requests
 import json
 from PyPDF2 import PdfReader 
+from flask import Flask, jsonify, send_from_directory
+from dotenv import load_dotenv
+
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 CORS(app)
 
 # Get OpenAI API key from environment variables
-api_key = ""
+load_dotenv()
+api_key = os.getenv("MY_API_KEY")
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    # Receive the uploaded file
-    file = request.files['file']
-    filename = file.filename
-    filepath = os.path.join('uploads', filename)
-    file.save(filepath)
+    try:
+        # Receive the uploaded file
+        file = request.files['file']
+        filename = file.filename
+        filepath = os.path.join('uploads', filename)
+        file.save(filepath)
 
-    # Convert the PDF to text
-    pdf_text = convert_pdf_to_text(filepath)
+        # Convert the PDF to text
+        pdf_text = convert_pdf_to_text(filepath)
 
-    # Send the text to ChatGPT API and get the response
-    response = chat_gpt_request(pdf_text)
+        # Send the text to ChatGPT API and get the response
+        response = chat_gpt_request(pdf_text)
 
-    # Emit the response back to the frontend
-    socketio.emit('response', response)
+        # Emit the response back to the frontend
+        socketio.emit('response', response)
 
-    return jsonify({"filename": filename})
+        return jsonify({"filename": filename})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 def convert_pdf_to_text(filepath):
     # Open the PDF file
@@ -47,27 +54,32 @@ def chat_gpt_request(text):
     # Define the prompt and other details for the GPT-4 model
     prompt = """Please analyze the provided exam PDF file and identify five specific categories that best represent the questions on the exam. Please ensure that the categories are not miscellaneous and are specific in representing the exam questions accurately.
 
-    After carefully reviewing the solutions of the exam, please provide a few tricks or things to be careful of when solving the problems. These should be specific to each category identified earlier. Additionally, if there are any equations that are frequently used in the exam, please list them and explain their relevance to the specific categories.
+    After carefully reviewing the solutions of the exam, please provide a few tricks or things to be careful of when solving the problems. These should be specific to each category identified earlier. Additionally, if there are any equations that are frequently used in the exam, please list them and explain their relevance to the specific categories according to the individual questions.
 
     Please note that your response should be detailed, accurate, and provide specific information for each category and associated tricks or equations.
     
-    Please Note that the responses should be formatted in json file syntax and should contain 5 categories under category. Then the section category should contains a section called "Questions" in which shows you which questions matched that catgeory(Note:Only one question per category, all category numbers must add up to orginal sum and will not always be in order), 
+    Please Note that the responses should be formatted in json file syntax and should contain 5 categories under category(nothing trivial like subject, name or type of question but more so around the concept, Ex: DO NTO USE true or false or multiple choice as a category). Then the section category should contains a section called "Questions" in which shows you which questions matched that category(Note:Only one question per category, all category numbers must ATLEAST add up to original sum and will not always be in order)
+    (this information should be extracted by the steps of the answer more so than then the question), 
     then under category should be "Tips and Tricks section" in 
-    which points out Non-trival facts a out how the problem was solved or a deeper explanation to keep in-mind when facing similar problems. Lastly under  category should be the formula that may serve, useful
-    for those type of question, not don't write down any problem specfic value formulas, they should all be general formulas that can also be found online, and written without special characters. 
+    which points out Non-trivial facts about how the problem was solved or a deeper explanation to keep in-mind when facing similar problems. Lastly under  category should be the formula that may serve, useful
+    for those types of questions under that topic(don't go beyond the scope of the problem)(please note that this can also be confirmed with the number before the paragraph of the last question), don't use any specific formulas with values from the problems, and  written without special characters. Finally a section in which teaches the category concept to someone who has never seen it before in about 5 sentences.
     Sample Output for one category in categories so u need 4 more:
-    "Categories": {
+    "Categories(EXACT NAME)": {
     "DO NOT COPY(Topics)": {
       "Questions Covered": [DO NOT COPY(info),
-      "Tips and Tricks": [
+      "Tips and Tricks(EXACT NAME)": [
         DO NOT COPY(Info)"
       ],
-      "Useful Formulas": [
+      "Useful Formulas(EXACT NAME)": [
         DO NOT COPY(Info)
       ]
+      "Category Summary(EXACT NAME)": [
+        DO NOT COPY(5 sentence Info)
+        ]
     }
 
     """
+
     headers = {
         "Authorization": f"Bearer {api_key}",
         "Content-Type": "application/json"
@@ -75,7 +87,7 @@ def chat_gpt_request(text):
     data = {
         "model": "gpt-4-0613",
         "messages": [
-            {"role": "system", "content": "You are an assistant specialized in analyzing exam patterns."},
+            {"role": "system", "content": "You are an assistant specialized in analyzing exam patterns. You only output JSON's, and no other extraneous text. Make sure to not factor in pages that include instructions or basic student information.When you get to naming things with the format I give you make sure you follow it and the names exactly like I give them."},
             {"role": "user", "content": f"Please analyze this exam that is mostly made from the text: {text}"},
             {"role": "user", "content": prompt}
         ]
@@ -110,6 +122,10 @@ def chat_gpt_request(text):
 def handle_process(filename):
     # This function can be used for other processing if needed
     pass
+
+@app.route('/getjson', methods=['GET'])
+def get_json():
+    return send_from_directory('responses', 'response.json')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5000)
--- react-websocket-file-upload/src/App.js
+++ react-websocket-file-upload/src/App.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import MyPieChart from './MyPieChart';  // Import the pie chart component
 import io from 'socket.io-client';
-import './App.css';
 
 const socket = io('http://localhost:5000');
 
 function App() {
   const [file, setFile] = useState(null);
-  const [processing, setProcessing] = useState(false);
-  const [resultsAvailable, setResultsAvailable] = useState(false);
   const [message, setMessage] = useState('');
 
   useEffect(() => {
@@ -18,11 +16,13 @@ function App() {
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
-    // handleUpload();
   };
 
+  const [showChart, setShowChart] = useState(false); 
+  const [fileUploadedTimestamp, setFileUploadedTimestamp] = useState(Date.now()); 
+ 
+
   const handleUpload = async () => {
-    setProcessing(true);
     const formData = new FormData();
     formData.append('file', file);
 
@@ -32,43 +32,21 @@ function App() {
     });
 
     const data = await response.json();
-    setProcessing(false);
-    setResultsAvailable(true);
     socket.emit('process', data.filename);
+    setShowChart(true); 
+    // Update the timestamp every time a file is uploaded
+    setFileUploadedTimestamp(Date.now());  
   };
 
   return (
     <div className="App">
-      <header>
-        <h1>Examlytics</h1>
-      </header>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
       <main>
-        { processing ? (
-          <div>
-            <h2>Explanation of website.</h2>
-              <div className="Drop-zone">
-                <h3>Processing your document...</h3>
-              <progress max="100" value="75"></progress>
-              </div>
-          </div>          
-        ) : (
-          resultsAvailable ? (
-            <div className="Results">
-              <p>Results</p>
-            </div>
-          ) : (
-            <div>
-              <h2>Explanation of website.</h2>
-              <div className="Drop-zone" ondrop={console.log("Hi")}>
-                <label for="upload-file" >Drag and drop a file or</label>
-                <input id="upload-file" className="button" type="file" accept=".pdf" onChange={handleFileChange} required />
-                <input className= "Upload-button" type="submit" value="Upload file" onClick={handleUpload} />
-              </div>
-            </div>
-          )
-        )}
+        {showChart && <MyPieChart lastUpdated={fileUploadedTimestamp} />}
       </main>
     </div>
+    
   );
 }
 
